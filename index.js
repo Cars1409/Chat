04f746221c242cb6520a7329b46c497bf04bead2
Cars1409 <101503416+Cars1409@users.noreply.github.com>
2022-09-17 20:24:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = requi('express');
 
 const app = express();
 
-app.use(express.static("public")); 
+app.use(express.static('public')); 
 
 app.listen(3000, () => {
     console.log('servidor en el puerto 3000');
